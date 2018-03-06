842149d9b6dd9c4ade5c3def46aa127285408b39
thongle7 <thong7@uw.edu>
2018-03-05 16:11:58 -0800

    adding server. R and ui.R

---
+++ server.R
@@ -0,0 +1,12 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+
+# function to filter the cereal data that we need
+my.server <- function(input, output) {
+
+
+  
+}
+shinyServer(my.server)
---
+++ ui.R
@@ -0,0 +1,18 @@
+my.ui <- fluidPage(
+  # Title of the page
+  titlePanel("Mass Shooting in United States"),
+  fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+
+        
+      ),
+      mainPanel(
+
+      )
+      
+    )
+    
+  )
+)
+shinyUI(my.ui)
