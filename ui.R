b00120b00e9a446a196a5196d953e965c3b2c731
T Le <thongle7@uw.edu>
2018-03-06 15:12:49 -0800

    change the UI layout

--- ui.R
+++ ui.R
@@ -1,26 +1,60 @@
-my.ui <- fluidPage(
-  # Title of the page
-  titlePanel("Mass Shooting in United States"),
-  fluidPage(
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("year", "Select year:", c( "All","2013", "2014", "2015", "2016", "2017")),
-        
-        sliderInput("Dead", "Dead Amount", min = 0, 
-                    max = 100, value = c(0,100)),
-        sliderInput("Injured", "Injured Amount", min = 0, 
-                    
-                    max = 442, value = c(0,442))
-        
-      ),
-      mainPanel(
-        leafletOutput(
-          outputId = "map"
-        )
-      )
-      
-    )
-    
-  )
-)
+my.ui <- fluidPage(fluidPage(navbarPage(
+  "Mass Shooting in United States",
+  tabPanel("Incidents Map",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput(
+                 "year",
+                 "Select year:",
+                 c("All", "2013", "2014", "2015", "2016", "2017")
+               ),
+               
+               sliderInput(
+                 "Dead",
+                 "Dead Amount",
+                 min = 0,
+                 max = 100,
+                 value = c(0, 100)
+               ),
+               sliderInput(
+                 "Injured",
+                 "Injured Amount",
+                 min = 0,
+                 max = 442,
+                 value = c(0, 442)
+               )
+               
+             ),
+             mainPanel(leafletOutput(outputId = "map", height = 800))
+             
+           )),
+  tabPanel("Heat Map",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput(
+                 "year",
+                 "Select year:",
+                 c("All", "2013", "2014", "2015", "2016", "2017")
+               ),
+               
+               sliderInput(
+                 "Dead",
+                 "Dead Amount",
+                 min = 0,
+                 max = 100,
+                 value = c(0, 100)
+               ),
+               sliderInput(
+                 "Injured",
+                 "Injured Amount",
+                 min = 0,
+                 max = 442,
+                 value = c(0, 442)
+               )
+               
+             ),
+             mainPanel()
+             
+           ))
+)))
 shinyUI(my.ui)
