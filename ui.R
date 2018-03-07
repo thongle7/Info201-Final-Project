b014de12d9612234b01ce0f169b895e22c49b29d
T Le <thongle7@uw.edu>
2018-03-07 13:13:26 -0800

    modified lat and lng in newdata, delete heatmap

--- ui.R
+++ ui.R
@@ -29,34 +29,6 @@ my.ui <- fluidPage(fluidPage(navbarPage(
              mainPanel(leafletOutput(outputId = "map", height = 800))
              
            )),
-  tabPanel("Heat Map",
-           sidebarLayout(
-             sidebarPanel(
-               selectInput(
-                 "year",
-                 "Select year:",
-                 c("All", "2013", "2014", "2015", "2016", "2017")
-               ),
-               
-               sliderInput(
-                 "Dead",
-                 "Dead Amount",
-                 min = 0,
-                 max = 100,
-                 value = c(0, 100)
-               ),
-               sliderInput(
-                 "Injured",
-                 "Injured Amount",
-                 min = 0,
-                 max = 442,
-                 value = c(0, 442)
-               )
-               
-             ),
-             mainPanel()
-             
-           )),
   tabPanel("Historgrams",
            sidebarLayout(
              sidebarPanel(
